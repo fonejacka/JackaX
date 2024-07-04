d8b87b1621fd2c95885328e1daac81f2a5669501
Sam Jacka <sam@thelogoguy.co.uk>
2024-07-04 15:27:06 +0100

    Fix deployment issues

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 // Use environment variable for MongoDB URI
