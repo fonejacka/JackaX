db120ed6cb1101e27622897a4574ec205ad2fb0d
Sam Jacka <sam@thelogoguy.co.uk>
2024-07-07 14:10:36 +0100

    Build frontend and include in deployment

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ app.get('*', (req, res) => {
 
 // Use environment variable for MongoDB URI
 const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://Sam:Popadopilis1%21@polarx.aad9alq.mongodb.net/?retryWrites=true&w=majority&appName=PolarX';
-mongoose.connect(mongoUri, {
-  serverSelectionTimeoutMS: 5000, // Wait up to 5 seconds for initial connection
+mongoose.connect(mongoUri, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000  // Wait up to 5 seconds for initial connection
 })
-.then(() => console.log('MongoDB connected successfully'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 
   
